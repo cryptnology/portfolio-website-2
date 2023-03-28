d15c417ddbb42feaacc48056e8a549ba1d15578e
cryptnology <jamie@cryptnology.dev>
2023-03-28 10:56:48 +0100

    Add font and colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
-      <main className='flex w-full flex-1 flex-col items-center justify-center px-20 text-center'>
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1>Porfolio Page</h1>
       </main>
-      <footer className='flex h-24 w-full items-center justify-center border-t mt-auto'>
+      <footer className="flex h-24 w-full items-center justify-center border-t mt-auto">
         <h1>Cryptnology.Dev</h1>
       </footer>
     </div>
